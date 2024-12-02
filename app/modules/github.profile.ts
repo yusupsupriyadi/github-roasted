777c8963f5133dca6211482c65fea5ba1bb06af1
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-02 13:51:50 +0700

    chore(github.profile): remove unused GitHub profile fetching function
    - Deleted the github.profile.ts file which contained an unused function for fetching GitHub user profiles.
    - This removal is part of a code cleanup to eliminate unnecessary code and streamline the application.

--- app/modules/github.profile.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function getGithubProfile(username: string) {
-	const response = await axios.get(`https://api.github.com/users/${username}`);
-	return response.data;
-}
