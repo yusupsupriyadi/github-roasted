3b36d7d11820b58fbc547ac5f9b97fc1e14aae2e
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-02 12:12:53 +0700

    chore(tailwind): remove unused font families from Tailwind configuration
    - Cleaned up the Tailwind CSS configuration by removing several unused font family entries.
    - This change helps streamline the configuration and reduces potential bloat in the CSS output.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,6 @@ export default {
 			fontFamily: {
 				sans: [
 					'Inter',
-					'ui-sans-serif',
-					'system-ui',
-					'sans-serif',
-					'Apple Color Emoji',
-					'Segoe UI Emoji',
-					'Segoe UI Symbol',
-					'Noto Color Emoji',
 				],
 			},
 			animation: {
