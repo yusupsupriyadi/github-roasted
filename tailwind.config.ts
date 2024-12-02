1adb29f505badbb9c91e29f6e25236fc73c10187
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-02 11:33:08 +0700

    feat(tailwind.config.ts): enhance configuration with custom animations and color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from 'tailwindcss';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette';
 
 export default {
 	content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
@@ -16,7 +19,41 @@ export default {
 					'Noto Color Emoji',
 				],
 			},
+			animation: {
+				spotlight: 'spotlight 2s ease .75s 1 forwards',
+			},
+			keyframes: {
+				spotlight: {
+					'0%': {
+						opacity: '0',
+						transform: 'translate(-72%, -62%) scale(0.5)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translate(-50%,-40%) scale(1)',
+					},
+				},
+			},
 		},
 	},
-	plugins: [],
+	plugins: [addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({
+	addBase,
+	theme,
+}: {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	addBase: any;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	theme: any;
+}) {
+	const allColors = flattenColorPalette(theme('colors'));
+	const newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+	);
+
+	addBase({
+		':root': newVars,
+	});
+}
