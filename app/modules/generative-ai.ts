d47e2139570c72ebb7eea69fd05dc0159797a479
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-02 13:51:45 +0700

    chore(generative-ai): remove unused AI generation function
    - Deleted the generative-ai.ts file which contained an unused function for generating AI responses based on GitHub profiles.
    - This removal is part of a code cleanup to eliminate unnecessary code and streamline the application.

--- app/modules/generative-ai.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { GithubProfile } from '~/components/types/GithubProfile.types';
-
-export async function generateAI({ body }: { body: GithubProfile }) {
-	const response = await axios.post('/api/roasting', body);
-	return response.data;
-}
