b5d68db98f238516bc340ffe2ff18330260187ae
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-02 11:33:20 +0700

    feat(GithubProfile.types): add type definition for GithubProfile

---
+++ app/components/types/GithubProfile.types.ts
@@ -0,0 +1,16 @@
+export type GithubProfile = {
+	name: string;
+	login: string;
+	bio: string;
+	location: string;
+	company: string;
+	blog: string;
+	followers: number;
+	following: number;
+	public_repos: number;
+	public_gists: number;
+	created_at: string;
+	updated_at: string;
+	twitter_username: string;
+	email: string;
+};
